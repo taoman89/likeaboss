7673b7ab3dceabdd8fc4f715427b96ee48b291b0
taoman89 <juntao.zhu.2010@sis.smu.edu.sg>
2013-02-20 23:08:20 +0800

    js file added plus enabled show / hide function
    Change-Id: Ie060cf57459b3305b6ac5590b937a52b391f075f
    Signed-off-by: taoman89 <juntao.zhu.2010@sis.smu.edu.sg>

---
+++ Like A Boss/war/media/inilist.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	$('#timetable').hide();
+	$('#showselection').hide();
+	
+	$('#hideselection').click(function(){
+		
+		$('#lovely-things-list').hide();
+		$('#timetable').show();
+		$('#showselection').show();
+		
+	});
+	$('#showselection').click(function(){
+		
+		$('#lovely-things-list').show();
+		$('#timetable').hide();
+		$('#showselection').hide();
+		
+	});
+    /*
+    * LOVELY THINGS
+    */
+
+    var options = {
+    		valueNames: [ 'name', 'description', 'category' ]
+    };
+
+    var featureList = new List('lovely-things-list', options);
+
+    $('#filter-compulsory').click(function() {
+        featureList.filter(function(item) {
+            if (item.values().category == "compulsory") {
+                return true;
+            } else {
+          
+                return false;
+            }
+        });
+        return false;
+    });
+
+    $('#filter-ge').click(function() {
+        featureList.filter(function(item) {
+            if (item.values().category == "ge") {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        return false;
+    });
+    $('#filter-none').click(function() {
+        featureList.filter();
+        return false;
+    });
+});
+
