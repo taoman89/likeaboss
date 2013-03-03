83af4d5417a68752fc13fc3bf87da369e1388bf7
taoman89 <juntao.zhu.2010@sis.smu.edu.sg>
2013-03-03 22:41:25 +0800

    Exam timetable autoamted
    Change-Id: Id3c0c383ce9419fd4f7f1205c2037a9e3b2c7b04
    Signed-off-by: taoman89 <juntao.zhu.2010@sis.smu.edu.sg>

---
+++ Like A Boss/src/boss/ExamDetailsServlet.java
@@ -0,0 +1,14 @@
+package boss;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExamDetailsServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
+}
--- Like A Boss/war/js/script.js
+++ Like A Boss/war/js/script.js
@@ -5,7 +5,7 @@ var slots;//slots that are first clicked by user
 
 
 //shows the slots that can be dragged into
-var showSlots = function (availSlots, backGroundColor,toggle){
+var showSlots = function (availSlots, backGroundColor){
 	var avail = availSlots.split(",");
 	//changes the css of desired slots into desired colours
 	for (var i = 0; i < avail.length; i ++){
@@ -20,12 +20,12 @@ var showSlots = function (availSlots, backGroundColor,toggle){
 		selected.css('background-color', backGroundColor);
 		//enables or disables the marked criteria
 		selected.attr('class').indexOf('mark');
-		if(toggle){
-			selected.toggleClass('mark');
-		}		
+		
+		selected.toggleClass('mark');
+		
 	}
 };
-
+// function that locks all of the timetable cells
 var lockAll = function(){
 	var table = document.getElementById("table2");
 	for (var i = 1, row; row = table.rows[i]; i++) {
@@ -37,6 +37,52 @@ var lockAll = function(){
 		 if(col.className.indexOf('mark')==-1){
 			 col.className = col.className + ' mark';
 		 }	   
+	   }
+	}
+	
+};
+
+var showExamTimeTable = function(){
+	
+	//hide all exam timetable
+	$('.exammod').each(function(){
+	    //if statement here 
+	    // use $(this) to reference the current div in the loop
+	    //you can try something like...
+
+	    if(!$(this).hasClass('hidden')){
+	    	$(this).toggleClass('hidden');
+	    }
+
+
+	 });
+	
+	
+	var table = document.getElementById("table2");
+	
+	//adding exam to exam table
+	for (var i = 1, row; row = table.rows[i]; i++) {
+	   //iterate through rows
+	   //rows would be accessed using the "row" variable assigned in the for loop
+	   for (var j = 1, col; col = row.cells[j]; j++) {
+	     //iterate through columns
+	     //see if the td is blank
+		 if(col.innerHTML!= null && col.innerHTML!= ''){
+			 var tdContent = col.innerHTML;
+			 
+			 var startIndex = tdContent.indexOf('class="code">');
+			 
+			 var examID = tdContent.substring(startIndex)
+			 var startIndex = examID.indexOf('>')+1;
+			 var endIndex = examID.indexOf('<');
+
+			 var examID = examID.substring(startIndex,endIndex);
+			 //examID is taken from the individual TD of the table
+			 
+			 if($('#'+examID).hasClass('hidden')){
+				 $('#'+examID).toggleClass('hidden');
+			 }
+		 }
 	   }
 	}
 	
@@ -59,6 +105,7 @@ $(document).ready(function() {
 	$('.drag').mouseup(function(){
 
 		showSlots(slots, initialColour,false);
+		lockAll();
 	});
 	
 });
@@ -74,7 +121,6 @@ var redipsInit,		// define redipsInit variable
 	report,			// function shows subject occurring in timetable
 	reportButton,	// show/hide report buttons
 	showAll,		// function show all subjects in timetable
-	printMessage,	// print message
 	divNodeList;	// node list of DIV elements in table2 (global variable needed in report() and visibility() function)
 
 
@@ -99,25 +145,8 @@ redipsInit = function () {
 			targetRow = targetCell.parentNode,	// target row
 			i, objNew;							// local variables
 		// if checkbox is checked and original element is of clone type then clone spread subjects to the week
-		
-		/** code not applicable **/
-		/*if (document.getElementById('week').checked === true && objOld.className.indexOf('clone') > -1) {
-			// loop through table cells
-			for (i = 0; i < targetRow.cells.length; i++) {
-				// skip cell if cell has some content (first column is not empty because it contains label)
-				if (targetRow.cells[i].childNodes.length > 0) {
-					continue;
-				}
-				// clone DIV element
-				objNew = rd.cloneObject(objOld);
-				// append to the table cell
-				targetRow.cells[i].appendChild(objNew);
-			}
-		}*/
-		// print message only if target and source table cell differ
-		if (rd.td.target !== rd.td.source) { 
-			printMessage('Content has been changed!');
-		}
+
+
 		// show / hide report buttons
 		/** code not applicable 
 		reportButton();**/
@@ -125,128 +154,15 @@ redipsInit = function () {
 		//returns the slots to the initial colour 
 		showSlots(slots, initialColour,true);
 		lockAll();
+		showExamTimeTable();
 	};
 
-	// after element is deleted from the timetable, print message
-	rd.event.deleted = function () {
-		printMessage('Content has been deleted!');
-		// show / hide report buttons
-		//reportButton();
-	};
 	
 	// if any element is clicked, then make all subjects in timetable visible
 	rd.event.clicked = function () {
 		showAll();
 	};
 };
-
-
-// save elements and their positions
-save = function () {
-	// scan timetable content
-	var content = REDIPS.drag.saveContent('table2');
-	// and save content
-	window.location.href = 'db_save.php?' + content;
-};
-
-
-// function shows subject occurring in timetable
-report = function (subject) {
-		// define day and time labels
-	var day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-		time = ['08:00', '09:00', '10:00', '11:00', '12:00',
-		        '13:00', '14:00', '15:00', '16:00'],
-		div = [],	// define array
-		cellIndex,	// cell index
-		rowIndex,	// row index
-		id,			// element id
-		i,			// loop variable
-		num = 0,	// number of found subject
-		str = '';	// result string
-	// show all elements
-	showAll();
-	// create array from node list (node list is global variable)
-	for (i = 0; i < divNodeList.length; i++) {
-		div[i] = divNodeList[i];
-	}
-	// sort div elements by the cellIndex (days in week) and rowIndex (hours)
-	div.sort(function (a, b) {
-		var a_ci = a.parentNode.cellIndex,				// a element cell index
-			a_ri = a.parentNode.parentNode.rowIndex,	// a element row index
-			b_ci = b.parentNode.cellIndex,				// b element cell index
-			b_ri = b.parentNode.parentNode.rowIndex;	// b element row index
-		return a_ci * 100 + a_ri - (b_ci * 100 + b_ri);
-	});
-	// loop goes through all collected elements
-	for (i = 0; i < div.length; i++) {
-		// define only first two letters of ID
-		// (cloned elements have appended c1, c2, c3 ...)
-		id = div[i].id.substr(0, 2);
-		// if id is equal to the passed subject then we have a match
-		if (id === subject) { 
-			// define cell index
-			cellIndex = div[i].parentNode.cellIndex;
-			// table row is parent element of table cell 
-			rowIndex = div[i].parentNode.parentNode.rowIndex;
-			// add line with found element
-			str += day[cellIndex - 1] + '\t\t' + time[rowIndex - 1] + '\n';
-			// increase counter
-			num++;
-		}
-		// other elements should be hidden
-		else {
-			div[i].style.visibility = 'hidden';
-		}
-	}
-	// if "Show report" is checked then show message
-	if (document.getElementById('report').checked === true) {
-		alert('Number of found subjects: ' + num + '\n' + str);
-	}
-};
-
-
-// show/hide report buttons
-reportButton = function () {
-	var	id,			// element id
-		i,			// loop variable
-		count,		// number of subjects in timetable
-		style,		// hidden or visible
-		// prepare subjects
-		subject = {'en': 0, 'ph': 0, 'ma': 0, 'bi': 0, 'ch': 0, 'it': 0, 'ar': 0, 'hi': 0, 'et': 0};
-	// loop goes through all collected elements
-	for (i = 0; i < divNodeList.length; i++) {
-		// define only first two letters of ID
-		// (cloned elements have appended c1, c2, c3 ...)
-		id = divNodeList[i].id.substr(0, 2);
-		// increase subject occurring
-		subject[id]++;
-	}
-	// loop through subjects
-	for (i in subject) {
-		// using the hasOwnProperty method to distinguish the true members of the object
-		if (subject.hasOwnProperty(i)) {
-			// prepare id of the report button
-			id = 'b_' + i;
-			// subject count on the timetable
-			count = subject[i];
-			if (count === 0) {
-				style = 'hidden';
-			}
-			else {
-				style = 'visible';
-			}
-			// hide or show report button
-			document.getElementById(id).style.visibility = style;
-		}
-	}
-};
-
-
-// print message
-printMessage = function (message) {
-	document.getElementById('message').innerHTML = message;
-};
-
 
 // function show all subjects in timetable
 showAll = function () {
