36737de7531eab2ba1d579abe0d20119c1734eee
taoman89 <juntao.zhu.2010@sis.smu.edu.sg>
2013-03-04 23:13:31 +0800

    half way through hover function
    Change-Id: I18577faa860e50a67d75d5c585ca1e6ba4d4ffac
    Signed-off-by: taoman89 <juntao.zhu.2010@sis.smu.edu.sg>

--- Like A Boss/war/js/script.js
+++ Like A Boss/war/js/script.js
@@ -212,6 +212,22 @@ var insertSection = function(){
 	}
 }
 
+
+//position the popup at the center of the page
+function positionPopup(){
+  if(!$("#overlay_form").is(':visible')){
+    return;
+  } 
+  $("#overlay_form").css({
+      left: ($(window).width() - $('#overlay_form').width()) / 2,
+      top: ($(window).width() - $('#overlay_form').width()) / 7,
+      position:'relative'
+  });
+}
+
+//maintain the popup at center of the page when browser resized
+//$(window).bind('resize',positionPopup);
+
 $(document).ready(function() {
 	initialColour = $('#row0').children(".col0").css('background-color');
 	
@@ -289,6 +305,19 @@ $(document).ready(function() {
 				lockAll();
 			});
 			
+			
+			//open popup
+			$('#'+ clonedID).mouseover(function(){
+			  $("#overlay_form").fadeIn(1000);
+			  positionPopup();
+			});
+			
+			//close popup
+			$('#'+ clonedID).mouseout(function(){
+				$("#overlay_form").fadeOut(500);
+			});
+
+
 			
 			//returns the slots to the initial colour 
 			showSlots(slots, initialColour);
